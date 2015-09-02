da507ea958412ffe84dcf3d79f5abdb35d68f958
Philip Thompson <ptnapoleon@gmail.com>
2015-09-02 15:29:54 -0400

    Revert "Add data from insert job for twcs/dtcs tests"
    This reverts commit c69e30eae43e1beb90643aefeeabaeeaade3848f. The generated json was too large for the graphing tool to adequately handle.

--- cass/twcs_dtcs/build.sh
+++ cass/twcs_dtcs/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../cass_stats.py -j ../../graph_v5/data/twcs_dtcs.brief.json -t "TWCS vs DTCS (brief)" --subtitle "" -f dtcs-brief.txt MIXED "dtcs" -f twcs-brief.txt MIXED "twcs" -f twcs-stress.txt INSERT "twcs" -f dtcs-stress.txt INSERT "dtcs"
+python ../cass_stats.py -j ../../graph_v5/data/twcs_dtcs.brief.json -t "TWCS vs DTCS (brief)" --subtitle "" -f dtcs-brief.txt MIXED "dtcs" -f twcs-brief.txt MIXED "twcs"
