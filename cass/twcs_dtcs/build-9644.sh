7c533ae4be06f8d48c500b59e9b0b7999eb45e3f
Philip Thompson <ptnapoleon@gmail.com>
2015-09-18 15:03:10 -0400

    Move 9644 data to its own graph

---
+++ cass/twcs_dtcs/build-9644.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python ../cass_stats.py -j ../../graph_v5/data/dtcs-9644.read.json -t "DTCS (read)" --subtitle "" -f 9644-read.txt READ "dtcs"
--- cass/twcs_dtcs/build-read.sh
+++ cass/twcs_dtcs/build-read.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../cass_stats.py -j ../../graph_v5/data/twcs_dtcs.read.json -t "TWCS vs DTCS (read)" --subtitle "" -f dtcs-read.txt READ "dtcs" -f twcs-read.txt READ "twcs" -f 9644-read.txt READ "dtcs-9644"
+python ../cass_stats.py -j ../../graph_v5/data/twcs_dtcs.read.json -t "TWCS vs DTCS (read)" --subtitle "" -f dtcs-read.txt READ "dtcs" -f twcs-read.txt READ "twcs"
